3947a48271e89592e493b68457ec6a645864898e
shams sarwari <shamsullahsarwari222@gmail.com>
2024-01-23 14:24:40 +0430

    add bookline-detail view

--- books/serializers.py
+++ books/serializers.py
@@ -11,9 +11,18 @@ class BookLineSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(source='book.slug')
     book_name = serializers.CharField(source='book.title')
     author_name = serializers.CharField(source='book.author')
+    home_image = serializers.SerializerMethodField()
     class Meta:
         model = BookLine
-        fields = ['id', 'slug', 'book_name','author_name', 'price']
+        fields = ['id', 'slug', 'book_name','author_name', 'price', 'home_image']
+
+    def get_home_image(self, obj):
+        image = obj.images.first()
+        if image:
+            return str(image.url)
+        else:
+            return None
+        
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -21,20 +30,25 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['title', 'image', 'description']
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        exclude = ['id', ]
+
 class BookSerializer(serializers.ModelSerializer):
-    author_name = serializers.CharField(source='author')
-    booklines = serializers.SerializerMethodField()
+    author = AuthorSerializer()
+    category_name = serializers.CharField(source='cateogry.title')
     class Meta:
         model = Book
-        fields = ['id', 'title', 'slug', 'author_name', 'booklines']
+        fields = ['id', 'title', 'slug', 'description','category_name', 'pub_date', 'author', 'num_of_views', 'reading_age']
 
-    def get_booklines(self, obj):
-        first_bookline = obj.booklines.first()
-        if first_bookline:
-            serialized_data = BookLineSerializer(first_bookline)
-            return serialized_data.data
-        else:
-            return None
-        
+    
+class BookLineDetailSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+    class Meta:
+        model = BookLine
+        fields = ['id','book', 'language', 'translator', 'price', 'stock_qty', 'num_of_pages']
+    
+
 
     
--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('categories', category_list, name='categories'),
     path('books', book_list, name='books'),
-    path('', bookline_list, name='book-lines'),
+    path('<int:pk>', bookline_detail, name='bookline'),
+    path('', bookline_list, name='booklines'),
 
 ]
