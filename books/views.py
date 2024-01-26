0f5b8ba54acb69ae06bf0984c26865c6030beda9
shams sarwari <shamsullahsarwari222@gmail.com>
2024-01-26 12:09:38 +0430

    add drf_sprectacular documentation

--- books/views.py
+++ books/views.py
@@ -3,61 +3,62 @@ from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-
-
 from .models import *
 from .permissions import AdminOrOwnerReview
 from .serializers import *
 
+
 # Create your views here.
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def category_list(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         cateogories = Category.objects.all()
         serialized_data = CategorySerializer(cateogories, many=True)
         return Response(serialized_data.data)
-    if request.method == 'POST':
+    if request.method == "POST":
         serialized_data = CategorySerializer(data=request.data)
         if serialized_data.is_valid():
             serialized_data.save()
             return Response(serialized_data.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serialized_data.errors)
-        
+
+
 @api_view()
 def test(request):
     return Response({"message": "welcome"})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def book_list(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         queryset = Book.objects.all()
         serialized_data = BookSerializer(queryset, many=True)
         return Response(serialized_data.data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def bookline_list(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         queryset = BookLine.objects.filter(add_to_page=True)
         serialized_data = BookLineSerializer(queryset, many=True)
         return Response(serialized_data.data)
-    
-@api_view(['GET'])
+
+
+@api_view(["GET"])
 def bookline_detail(request, pk):
     queryset = get_object_or_404(BookLine, id=pk)
-    if request.method == 'GET':
+    if request.method == "GET":
         serialized_data = BookLineDetailSerializer(queryset)
         return Response(serialized_data.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def related_books(request, pk):
     category = get_object_or_404(Category, id=pk)
     booklines = BookLine.objects.filter(
-        Q(book__category=category) &
-        Q(add_to_page=True)
-        )
+        Q(book__category=category) & Q(add_to_page=True)
+    )
     if len(booklines) < 10:
         queryset = booklines
     else:
@@ -66,45 +67,65 @@ def related_books(request, pk):
     serialized_data = BookLineSerializer(queryset, many=True)
     return Response(serialized_data.data)
 
-  
-@api_view(['GET'])
+
+@api_view(["GET"])
 def related_booklines(request, pk):
     bookline = get_object_or_404(BookLine, id=pk)
-    related_booklines = BookLine.objects.filter(book=bookline.book).exclude(id=bookline.id)
+    related_booklines = BookLine.objects.filter(book=bookline.book).exclude(
+        id=bookline.id
+    )
 
     serialized_data = BookLineSerializer(related_booklines, many=True)
     return Response(serialized_data.data)
 
-@api_view(['GET', 'POST'])
+
+@api_view(["GET", "POST"])
 def book_reviews(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    if request.method == 'GET':
+    if request.method == "GET":
         reviews = Review.objects.filter(book=book)
         serialized_data = ReviewSerializer(reviews, many=True)
         return Response(serialized_data.data)
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         serialized_data = ReviewSerializer(data=request.data)
         if serialized_data.is_valid():
             serialized_data.save(profile=request.user.profile, book=book)
-            return Response({'message': 'review created successfully'}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"message": "review created successfully"},
+                status=status.HTTP_201_CREATED,
+            )
         else:
             return Response(serialized_data.errors)
-        
-    
-@api_view(['PATCH', 'DELETE'])
+
+
+@api_view(["PATCH", "DELETE"])
 @permission_classes([AdminOrOwnerReview])
 def review_detail(request, review_id):
     review = get_object_or_404(Review, id=review_id)
-    if request.method == 'PATCH':
-        if request.user.is_superuser or request.user.profile == review.profile:
-            comment = request.data.get('comment')
+    if request.user.is_superuser or request.user.profile == review.profile:
+        if request.method == "PATCH":
+            comment = request.data.get("comment")
             if comment:
                 review.comment = comment
                 review.save()
-                return Response({'message': 'successully updated'}, status=status.HTTP_200_OK)
+                return Response(
+                    {"message": "successully updated"}, status=status.HTTP_200_OK
+                )
 
             else:
-                return Response({'error': 'error occured'}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'message': 'You are not allowed to perform this action'}, status=status.HTTP_403_FORBIDDEN)
+                return Response(
+                    {"error": "error occured"}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+        if request.method == "DELETE":
+            review.delete()
+            return Response(
+                {"message": "review deleted successfully"}, status=status.HTTP_200_OK
+            )
+
+    else:
+        return Response(
+            {"message": "You are not allowed to perform this action"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
--- config/settings.py
+++ config/settings.py
@@ -21,99 +21,98 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)^3*1glwx6h1^36j*^umh7$6^%b21x*u*t02#@8#_vlkzc7zt('
+SECRET_KEY = "django-insecure-)^3*1glwx6h1^36j*^umh7$6^%b21x*u*t02#@8#_vlkzc7zt("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # custom apps:
-    'books.apps.BooksConfig',
-    'accounts.apps.AccountsConfig',
-    'orders.apps.OrdersConfig',
-    
-
+    "books.apps.BooksConfig",
+    "accounts.apps.AccountsConfig",
+    "orders.apps.OrdersConfig",
     # 3rd parties:
-    'rest_framework',
-    'corsheaders',
-
+    "rest_framework",
+    "corsheaders",
+    "drf_spectacular",
 ]
 
-AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # 3rd party:
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-DATABASES['default'] = dj_database_url.parse("postgres://bookstore_y1io_user:zBzGxVDbW1SGWRl9cp68jqBMzbdDu4Bp@dpg-cmmfcka1hbls73cfl30g-a.oregon-postgres.render.com/bookstore_y1io")
+DATABASES["default"] = dj_database_url.parse(
+    "postgres://bookstore_y1io_user:zBzGxVDbW1SGWRl9cp68jqBMzbdDu4Bp@dpg-cmmfcka1hbls73cfl30g-a.oregon-postgres.render.com/bookstore_y1io"
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -121,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -133,16 +132,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
-STATICFILES_DIRS = (BASE_DIR.joinpath('static'),)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.joinpath('media')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR.joinpath("staticfiles")
+STATICFILES_DIRS = (BASE_DIR.joinpath("static"),)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR.joinpath("media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Bookstore",
+}
