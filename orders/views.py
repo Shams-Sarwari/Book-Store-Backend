260fed99bb2ee2f8925392ca364d1a3338bd9d0b
shams sarwari <shamsullahsarwari222@gmail.com>
2024-01-30 11:34:24 +0430

    if the item is already in the cart so increase the quantity instead of creating new item

--- orders/views.py
+++ orders/views.py
@@ -21,17 +21,31 @@ def get_cart_items(request):
 @permission_classes([IsAuthenticated])
 def add_to_cart(request, bookline_id):
     if request.method == "POST":
+        user_cart = get_object_or_404(Cart, profile=request.user.profile)
         book_line = get_object_or_404(BookLine, id=bookline_id)
+
         serialized_data = CartItemSerializer(data=request.data)
         if serialized_data.is_valid():
+            # check if the quantity has been sent from the user, if not set 1
             try:
                 quantity = serialized_data.validated_data["quantity"]
             except:
                 quantity = 1
+            # price will automatically set by the multiplication of quantity and item price
             price = quantity * book_line.price
-            serialized_data.save(
-                cart=request.user.profile.cart, book_line=book_line, price=price
-            )
+
+            # first check if the item is already in cart, if so just increase the quantity
+            if CartItem.objects.filter(cart=user_cart, book_line=book_line).exists():
+                cart_item = CartItem.objects.get(cart=user_cart, book_line=book_line)
+                print(cart_item)
+                cart_item.quantity += quantity
+                cart_item.price += price
+                cart_item.save()
+            # if the item is not in the cart so add it.
+            else:
+                serialized_data.save(
+                    cart=request.user.profile.cart, book_line=book_line, price=price
+                )
             return Response(
                 {"message": "successfully added to cart"},
                 status=status.HTTP_201_CREATED,
