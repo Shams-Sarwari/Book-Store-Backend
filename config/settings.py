fb5757080bc35b6fe807325857475d6e35c4e819
shams sarwari <shamsullahsarwari222@gmail.com>
2024-02-14 12:10:34 +0430

    add google authentication setting to settings

--- config/settings.py
+++ config/settings.py
@@ -168,3 +168,8 @@ SPECTACULAR_SETTINGS = {
 
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+# social authentication
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
